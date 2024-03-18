5cc1c867fca09a15da08f866c33d1f989c5c6f1c
Vijay Nadkarni <vnadkarni@gmail.com>
2024-03-18 22:51:08 +0000

    first commit

---
+++ src/main/java/com/vnadkarni/tfl/tflkafka/TflkafkaApplication.java
@@ -0,0 +1,13 @@
+package com.vnadkarni.tfl.tflkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TflkafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TflkafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vnadkarni/tfl/tflkafka/TflkafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vnadkarni.tfl.tflkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TflkafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
